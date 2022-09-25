fb8250b8034ba099289f98baebc69f91d7362ea1
Matheus Henrique <19matheushenrique03@gmail.com>
2022-09-25 14:52:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,8 @@ let key6 = document.querySelector('#key-6');
 let key7 = document.querySelector('#key-7');
 let key8 = document.querySelector('#key-8');
 let key9 = document.querySelector('#key-9');
-let key10 = document.querySelector('#key-10');
-let key11 = document.querySelector('#key-11');
 
-const keyBoard = [sum,subtraction,multiplication,division,percentage,key0,key1,key2,key3,key4,key5,key6,key7,key8,key9,key10,key11];
+const keyBoard = [sum,subtraction,multiplication,division,percentage,key0,key1,key2,key3,key4,key5,key6,key7,key8,key9];
 
 const filterKeyBoard = keyBoard.filter(keys => keys.addEventListener('click',(() => {
     account.innerHTML += keys.innerHTML
